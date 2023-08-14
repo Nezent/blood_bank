1a6de0d707a6fe60ff2e73b0b1a56afc4ec4cbf4
Sirajum Munir <nezentbd@gmail.com>
2023-08-14 09:13:31 +0600

    Notification Screen

--- lib/views/notification_screen.dart
+++ lib/views/notification_screen.dart
@@ -13,33 +13,152 @@ class _NotificationScreenState extends State<NotificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Notifications',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  'Marked as Seen',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            NotificationItem(
+                icon: Icon(Icons.account_box),
+                value: 'Dipto wants B+ Blood near you'),
+            NotificationItem(
+                icon: Icon(Icons.bloodtype),
+                value: 'You have an appointment on 5.30 pm'),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(4),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundImage: AssetImage('images/main.jpg'),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.64,
+                            child: Text(
+                              'Pinky wants to be your friend',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class NotificationItem extends StatelessWidget {
+  final String value;
+  final Icon icon;
+  const NotificationItem({
+    super.key,
+    required this.icon,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 1,
+              blurRadius: 2,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
           child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                'Sorry for the incovenience',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    color: Palette.primaryRed),
+              Row(
+                children: [
+                  icon,
+                  SizedBox(
+                    width: 16,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.64,
+                    child: Text(
+                      value,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                'Working On Process',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Palette.primaryRed,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Text(
+                    '1',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
                 ),
               ),
             ],
           ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
